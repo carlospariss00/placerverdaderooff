ad8148859f520b8c23974c8c81d18bf187179d2f
carlos <pariscarloss3@gmail.com>
2025-10-02 14:02:54 -0500

    mi ultimo commit

---
+++ functions/videos/filename.js
@@ -0,0 +1,17 @@
+export async function onRequest(context) {
+  const { env, params } = context;
+
+  // El nombre del archivo viene de la URL, ejemplo: /videos/demo.mp4
+  const object = await env.mis-videos2.get(params.filename);
+
+  if (!object) {
+    return new Response("Archivo no encontrado", { status: 404 });
+  }
+
+  return new Response(object.body, {
+    headers: {
+      "Content-Type": object.httpMetadata?.contentType || "video/mp4",
+      "Cache-Control": "public, max-age=3600" // cache en Cloudflare 1 hora
+    }
+  });
+}
