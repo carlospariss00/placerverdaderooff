d1bb27cbb72bbd99487367bc955d39cd82b38d03
carlospariss00 <pariscarloss3@gmail.com>
2025-10-02 14:25:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Array con la información de todos los videos
 const videos = [
-  { id: 1, title: "Video 0", src: "/videos/video00.mp4", thumb: "videos/video1.mp4" },
-  { id: 1, title: "Video 1", src: "/videos/video01.mp4", thumb: "videos/video1.mp4" },
-  { id: 2, title: "Video 2", src: "/videos/video02.mp4", thumb: "videos/video2.mp4" },
-  { id: 3, title: "Video 3", src: "/videos/video03.mp4", thumb: "videos/video3.mp4" },
-  { id: 4, title: "Video 4", src: "/videos/video04.mp4", thumb: "videos/video4.mp4" }, 
-  { id: 5, title: "Video 5", src: "/videos/video05.mp4", thumb: "videos/video5.mp4" },
-  { id: 6, title: "Video 6", src: "/videos/video06.mp4", thumb: "videos/video6.mp4" },
-  { id: 7, title: "Video 7", src: "/videos/video07.mp4", thumb: "videos/video7.mp4" },
-  { id: 8, title: "Video 8", src: "/videos/video08.mp4", thumb: "videos/video8.mp4" },
-  { id: 9, title: "Video 9", src: "/videos/video09.mp4", thumb: "videos/video9.mp4" },
-  { id: 10, title: "Video 10", src: "/videos/video10.mp4", thumb: "videos/video10.mp4" },
+  { id: 1, title: "Video 0", src: "video00.mp4", thumb: "videos/video1.mp4" },
+  { id: 1, title: "Video 1", src: "video01.mp4", thumb: "videos/video1.mp4" },
+  { id: 2, title: "Video 2", src: "video02.mp4", thumb: "videos/video2.mp4" },
+  { id: 3, title: "Video 3", src: "video03.mp4", thumb: "videos/video3.mp4" },
+  { id: 4, title: "Video 4", src: "video04.mp4", thumb: "videos/video4.mp4" }, 
+  { id: 5, title: "Video 5", src: "video05.mp4", thumb: "videos/video5.mp4" },
+  { id: 6, title: "Video 6", src: "video06.mp4", thumb: "videos/video6.mp4" },
+  { id: 7, title: "Video 7", src: "video07.mp4", thumb: "videos/video7.mp4" },
+  { id: 8, title: "Video 8", src: "video08.mp4", thumb: "videos/video8.mp4" },
+  { id: 9, title: "Video 9", src: "video09.mp4", thumb: "videos/video9.mp4" },
+  { id: 10, title: "Video 10", src: "video10.mp4", thumb: "videos/video10.mp4" },
 
   // Agrega más videos aquí simplemente añadiendo nuevas entradas al array
 ];
@@ -45,4 +45,5 @@ function loadVideoGallery() {
 }
 
 // Cargar la galería cuando la página esté lista
+
 document.addEventListener('DOMContentLoaded', loadVideoGallery);
