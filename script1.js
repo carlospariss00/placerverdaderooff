ffc69b98afc61ef13624604ad9b22b451c573ae6
carlospariss00 <pariscarloss3@gmail.com>
2025-11-16 16:08:16 -0500

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-// Obtener el parámetro 'src' de la URL
-const urlParams = new URLSearchParams(window.location.search);
-const src = urlParams.get('src');
-if(src) {
-  document.getElementById('player').src = src;
-} else {
-  document.body.innerHTML = '<p style="color:white;">No se encontró el video.</p>';
-}
